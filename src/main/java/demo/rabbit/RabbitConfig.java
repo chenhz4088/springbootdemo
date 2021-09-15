43316f43e3876b4501dbe71b01e14161748d034a
chen <804805928@qq.com>
2021-09-15 23:15:16 +0800

    新增项目

---
+++ src/main/java/demo/rabbit/RabbitConfig.java
@@ -0,0 +1,84 @@
+package demo.rabbit;
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+
+@Configuration
+public class RabbitConfig {
+    public static final String X_EXCHANGE = "X";
+    public static final String QUEUE_A = "QA";
+    public static final String QUEUE_C = "QC";
+    public static final String QUEUE_B = "QB";
+    public static final String Y_DEAD_LETTER_EXCHANGE = "Y";
+    public static final String DEAD_LETTER_QUEUE = "QD";
+
+    @Bean("xExchange")
+    public DirectExchange xExchange(){
+        return new DirectExchange(X_EXCHANGE);
+    }
+
+    @Bean("yExchange")
+    public DirectExchange yExchange(){
+        return new DirectExchange(Y_DEAD_LETTER_EXCHANGE);
+    }
+
+    @Bean("queueA")
+    public Queue queueA(){
+        HashMap<String,Object> args = new HashMap<>(3);
+        args.put("x-dead-letter-exchange",Y_DEAD_LETTER_EXCHANGE);
+        args.put("x-dead-letter-routing-key","YD");
+        args.put("x-message-ttl",10000);
+        return QueueBuilder.durable(QUEUE_A).withArguments(args).build();
+    }
+
+    @Bean("queueB")
+    public Queue queueB(){
+        HashMap<String,Object> args = new HashMap<>(3);
+        args.put("x-dead-letter-exchange",Y_DEAD_LETTER_EXCHANGE);
+        args.put("x-dead-letter-routing-key","YD");
+        args.put("x-message-ttl",40000);
+        return QueueBuilder.durable(QUEUE_B).withArguments(args).build();
+    }
+
+    @Bean("queueC")
+    public Queue queueC(){
+        HashMap<String,Object> args = new HashMap<>(3);
+        args.put("x-dead-letter-exchange",Y_DEAD_LETTER_EXCHANGE);
+        args.put("x-dead-letter-routing-key","YD");
+        return QueueBuilder.durable(QUEUE_C).withArguments(args).build();
+    }
+
+    @Bean
+    public Binding queueaBindingX(@Qualifier("queueA")Queue queueA,
+                                  @Qualifier("xExchange")DirectExchange xExchange){
+        return BindingBuilder.bind(queueA).to(xExchange).with("XA");
+    }
+
+    @Bean
+    public Binding queuebBindingX(@Qualifier("queueB")Queue queueB,
+                                  @Qualifier("xExchange")DirectExchange xExchange){
+        return BindingBuilder.bind(queueB).to(xExchange).with("XB");
+    }
+
+    @Bean
+    public Binding queuecBindingX(@Qualifier("queueC")Queue queueC,
+                                  @Qualifier("xExchange")DirectExchange xExchange){
+        return BindingBuilder.bind(queueC).to(xExchange).with("XC");
+    }
+
+    @Bean("queueD")
+    public Queue queueD(){
+        return new Queue(DEAD_LETTER_QUEUE);
+    }
+
+
+    @Bean
+    public Binding deadLetterBindingQAD(@Qualifier("queueD") Queue queueD,
+                                        @Qualifier("yExchange") DirectExchange yExchange){
+        return BindingBuilder.bind(queueD).to(yExchange).with("YD");
+    }
+}
---
+++ src/main/java/demo/rabbit/SendMsgController.java
@@ -0,0 +1,44 @@
+package demo.rabbit;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.DateTimeException;
+import java.util.Date;
+
+@RestController
+@Slf4j
+@RequestMapping("ttl")
+public class SendMsgController {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @GetMapping("sendMsg/{msg}")
+    public void sendMsg(@PathVariable String msg){
+        log.info("当前时间：{},发送一条信息给两个 TTL 队列:{}", new Date(), msg);
+        rabbitTemplate.convertAndSend("X", "XA", "消息来自 ttl 为 10S 的队列: "+msg);
+        rabbitTemplate.convertAndSend("X", "XB", "消息来自 ttl 为 40S 的队列: "+msg);
+    }
+
+    @GetMapping("sendMsg/{msg}/{time}")
+    public void sendMsgOfTime(@PathVariable String msg,@PathVariable String time){
+        log.info("当前时间：{},发送一条信息给两个 TTL 队列:{}", new Date(), msg);
+        rabbitTemplate.convertAndSend("X", "XC", "消息来自 ttl 为"+time+"S 的队列: "+msg,(correlation)->
+        {
+            correlation.getMessageProperties().setExpiration(time);
+            return correlation;
+        }
+        );
+//        rabbitTemplate.convertAndSend("X", "XB", "消息来自 ttl 为 40S 的队列: "+msg);
+    }
+
+
+
+}
